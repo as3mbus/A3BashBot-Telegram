345af2c9dae0764d9dc75e554bed065bec040b65
as3mbus <asembus@gmail.com>
2018-01-28 21:33:03 +0700

    replying all message recieved in chat where bot exist

--- config.sh
+++ config.sh
@@ -1,9 +1,8 @@
 token=#insert telegram bot token here
 tele_url="https://api.telegram.org/bot${token}"
-curl -s "${tele_url}/getMe"
-curl -s "${tele_url}/getUpdates"| json_reformat
-curl -s "${tele_url}/getUpdates" | jq -r ".result"
-curl -s "${tele_url}/getUpdates" | jq -r ".result[].message.chat.id"
-curl -s "${tele_url}/sendMessage?chat_id=${chat_id}" \
-  --data-urlencode "text=I Am BOT Hello Human" | json_reformat
-curl -s "${tele_url}/getUpdates?offset=1517064599"| json_reformat
+#curl -s "${tele_url}/getMe"
+#curl -s "${tele_url}/getUpdates"| json_reformat
+#curl -s "${tele_url}/getUpdates" | jq -r ".result"
+#curl -s "${tele_url}/getUpdates" | jq -r ".result[].message.chat.id"
+#curl -s "${tele_url}/sendMessage?chat_id=${chat_id}" --data-urlencode "text=I Am BOT Hello Human" | json_reformat
+#curl -s "${tele_url}/getUpdates?offset=1517064599"| json_reformat
--- main.sh
+++ main.sh
@@ -16,15 +16,37 @@ fi
 tele_url="https://api.telegram.org/bot${token}"
 
 ### -- task -- 
-
+# get updates from telegram bot through curl request
 function process_observe() {
-  local i update
+    local i update
+    # updates store every response from server
+    local updates=$(curl -s "${tele_url}/getUpdates")
+    # count number of message / update from updates response earlier
+    local count_update=$(echo $updates | jq -r ".result | length") 
+    
+    # iterate through update and echo it
+    for ((i=0; i<$count_update; i++)); do
+        update=$(echo $updates | jq -r ".result[$i]")
+        echo "$update\n"
+    done
+}
+# send reply to every message ?
+function process_reply() {
+  local i update message_id chat_id
     local updates=$(curl -s "${tele_url}/getUpdates")
     local count_update=$(echo $updates | jq -r ".result | length") 
     
     for ((i=0; i<$count_update; i++)); do
         update=$(echo $updates | jq -r ".result[$i]")
-        echo "$update\n"
+    
+        message_id=$(echo $update | jq -r ".message.message_id")     
+        chat_id=$(echo $update | jq -r ".message.chat.id") 
+           
+        result=$(curl -s "${tele_url}/sendMessage" \
+                  --data-urlencode "chat_id=${chat_id}" \
+                  --data-urlencode "reply_to_message_id=${message_id}" \
+                  --data-urlencode "text=Thank you for your message."
+            );
     done
 }
 
@@ -34,7 +56,10 @@ function do_observe() {
   # no loop
     process_observe
 } 
+function do_reply() {
+    process_reply
+}
 
 ### -- main --
 
-do_observe
+do_reply
